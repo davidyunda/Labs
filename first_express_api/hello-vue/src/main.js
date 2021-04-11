9c7bbc0a42328fb902c20802fb275249d5fd1a4d
davidyunda <77813936+davidyunda@users.noreply.github.com>
2021-04-11 17:36:25 -0500

    Delete first_express_api directory

--- first_express_api/hello-vue/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import HelloAPI from '@/services/HelloAPI'
-
-let app= createApp(App)
-
-app.config.globalProperties.$hello_api = HelloAPI
-
-app.mount('#app')
--- first_express_api/routes/routes.js
+++
@@ -1,8 +0,0 @@
-let express = require('express')
-let router = express.Router()
-
-router.get('/', function(req, res, next) {
-    res.json( { 'message': 'Hello! ITEC 2560 '} )
-})
-
-module.exports = router
